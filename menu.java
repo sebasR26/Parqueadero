de6fe7ea14784deca591a14a4da8c7189546cf0a
sebasR26 <sebassrp47@gmail.com>
2026-08-20 21:00:11 -0500

    correccion de dimension

---
+++ menu.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class menu {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        metodos m = new metodos();
+        boolean continuar=true;
+        objmatriz[][] a = new objmatriz[2][2];
+        
+        while (continuar) {
+            System.out.println("\nBienvenido, que quiere?\n");
+            System.out.println("1- llenar parqueadero");
+            System.out.println("2- mostrar parqueadero");
+            int opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    a = m.llenarMatriz(a, sc);
+                    
+                    break;
+                case 2:
+                    m.mostrarMatriz(a);
+            
+                default:
+                    break;
+            }
+            
+        }
+    }
+}
